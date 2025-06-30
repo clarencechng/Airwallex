74a1dacab9462860f618abf561c49a591c7eb7e7
Clarence Chng <chng.clarence@gmail.com>
2025-07-01 06:53:12 +0800

    Initial Commit

---
+++ Airwallex/AirwallexApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirwallexApp.swift
+//  Airwallex
+//
+//  Created by Clarence Chng on 1/7/25.
+//
+
+import SwiftUI
+
+@main
+struct AirwallexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
